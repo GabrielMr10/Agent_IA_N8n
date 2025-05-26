ffed8c0485f1e2f0bf50f013828cebf565bd22aa
Gabriel Meira <gabrielmendonca1199@gmail.com>
2025-05-26 14:12:06 -0300

    configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Agent_IA_N8n/",
   server: {
     host: "::",
     port: 8080,
